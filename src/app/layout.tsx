12d0da1a6490d4b3568739b379b33e3a08587d37
Nicholas Smith <nsmith9@gatech.edu>
2026-08-18 21:34:40 -0400

    Load Boundary Frame presentation layer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./portfolio.css";
+import "./boundary-frame.css";
 
 export const metadata: Metadata = {
   title: {
