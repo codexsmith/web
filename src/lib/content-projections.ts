357e738599e8b7a650fd5a186806935e1efe35f6
Nicholas Smith <nsmith9@gatech.edu>
2026-08-18 21:12:41 -0400

    Hydrate supporting Research projection

--- src/lib/content-projections.ts
+++ src/lib/content-projections.ts
@@ -1,7 +1,8 @@
 import type { ContentNode } from "@/lib/content";
 import { hydrateAboutNode } from "@/lib/about-content";
 import { hydratePublicInterestNode } from "@/lib/public-interest-content";
+import { hydrateResearchNode } from "@/lib/research-content";
 
 export function hydrateContentNode(node: ContentNode): ContentNode {
-  return hydratePublicInterestNode(hydrateAboutNode(node));
+  return hydrateResearchNode(hydratePublicInterestNode(hydrateAboutNode(node)));
 }
