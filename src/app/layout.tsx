bc0a852d7efea27e5c28a178df49991d339052b6
Nicholas Smith <nsmith9@gatech.edu>
2026-08-22 10:26:46 -0400

    Load local navigation instrument refinement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ import "./frame-spatial-contract.css";
 import "./trace-nav-refinement.css";
 import "./card-root-control-board.css";
 import "./card-secondary-control-board.css";
+import "./local-nav-instrument.css";
 import "./apparatus-prototype.css";
 import "./apparatus-prototype-hardening.css";
 import "./apparatus-sleek-refinement.css";
