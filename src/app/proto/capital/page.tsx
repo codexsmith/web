1e800fe497b11a25d09222ed8ece1b68f0114a3a
Nicholas Smith <nsmith9@gatech.edu>
2026-08-31 23:10:13 -0400

    Load end-interface contact refinement

--- src/app/proto/capital/page.tsx
+++ src/app/proto/capital/page.tsx
@@ -7,6 +7,7 @@ import "./capital-frame-manifolds.css";
 import "./capital-frame-connector-interfaces.css";
 import "./capital-frame-canonical-connectors.css";
 import "./capital-frame-side-interface-fit.css";
+import "./capital-frame-end-interface-contact.css";
 
 export const metadata: Metadata = {
   title: "Boundary First Labs · Capital Economics Prototype",
