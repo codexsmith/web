ac66cc1618006b50709fb049f84abd06ca605aba
Nicholas Smith <nsmith9@gatech.edu>
2026-09-01 00:22:04 -0400

    Polish compact tour card

--- src/components/bfux/PhysicalMachineExperience.tsx
+++ src/components/bfux/PhysicalMachineExperience.tsx
@@ -9,6 +9,7 @@ import "./five-minute-tour-card.css";
 import "./five-minute-tour-growth.css";
 import "./five-minute-tour-compact.css";
 import "./five-minute-tour-about-attachment.css";
+import "./five-minute-tour-compact-polish.css";
 
 const resolutionStorageKey = "bfl_lab_machine_resolution";
 
