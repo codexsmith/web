b1be773f534dfa98f2612836730856fa73913126
Nicholas Smith <nsmith9@gatech.edu>
2026-09-11 03:36:55 -0400

    Load scoped billboard density polish

--- src/components/bfux/RepresentationLabBillboardCard.tsx
+++ src/components/bfux/RepresentationLabBillboardCard.tsx
@@ -3,6 +3,7 @@
 import { representationLabBillboardNode } from "./lab-machine-model";
 import "./representation-lab-billboard-card.css";
 import "./representation-lab-billboard-layout.css";
+import "./representation-lab-billboard-polish.css";
 
 export function RepresentationLabBillboardCard() {
   const node = representationLabBillboardNode;
