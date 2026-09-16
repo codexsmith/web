718f44d05b32b51c751a59ac329c433707bdd620
Nicholas Smith <nsmith9@gatech.edu>
2026-09-16 00:33:59 -0400

    Load Full Forge proportion calibration after mockup fidelity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,6 +80,7 @@ import "./p18-core-projection-style-parity.css";
 import "./p19-products-responsive-accordion.css";
 import "./p20-lab-machine-physical-process.css";
 import "../components/bfux/lab-machine-mockup-fidelity.css";
+import "../components/bfux/forge-proportion-calibration.css";
 import "./machine-view-controls.css";
 import "./machine-card-flight.css";
 
